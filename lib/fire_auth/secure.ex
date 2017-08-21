6b2113d063286d48596905ce7d834ee454da0265
FritzFlorian <flo.fritz@t-online.de>
2017-08-22 01:34:04 +0200

    Refactor Secure Plug

--- lib/fire_auth/secure.ex
+++ lib/fire_auth/secure.ex
@@ -1,31 +1,49 @@
 defmodule FireAuth.Secure do
+  @moduledoc """
+  Secures routes.
+  Requires FireAuth plug to be called before.
+  Can optionally restrict route access to a specific user group.
+
+  secure with group:
+  plug FireAuth.Secure, group: "required_group"
+   
+  only for authorized users:
+  plug FireAuth.Secure
+
+  only secure some routes
+  plug FireAuth.Secure when action in [:index]
+  """
   import Plug.Conn
 
   def init(opts) do
-    opts
+    case Keywod.fetch(opts, :group) do
+      {:ok, group} ->
+        %{group: group}
+      _ ->
+        %{}
+    end
   end
 
-  def call(conn, opts) do
-    if conn.assigns[:authenticated] do
-      case Keyword.fetch(opts, :group) do
-        {:ok, group} ->
-          user_groups = conn.assigns[:groups]
-          if Enum.member?(user_groups || [], group) do
-            conn
-          else
-            conn
-            |> put_status(:forbidden)
-            |> Phoenix.Controller.json(%{error: "Forbidden!"})
-            |> halt()
-          end
-        _ -> 
+  def call(%{assigns: %{fire_auth: %{authenticated: true} = fire_auth}} = conn, opts) do
+    case opts do
+      %{grop: group} ->
+        if Enum.member?(fire_auth.groups , group) do
           conn
-      end
-    else
-      conn
-      |> put_status(:unauthorized)
-      |> Phoenix.Controller.json(%{error: "Authentication Required!"})
-      |> halt()
+        else
+          conn
+            |> put_resp_content_type("application/json")
+            |> resp(:forbidden, "{\"error\": \"Forbidden!\"}")
+            |> halt()
+        end
+      _ -> 
+        conn
     end
+  end
+
+  def call(conn, _) do
+    conn
+      |> put_resp_content_type("application/json")
+      |> resp(:unauthorized, "{\"error\": \"Authentication Required!\"}")
+      |> halt()
   end
 end
