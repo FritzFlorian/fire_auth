41712d53bd431d496708b114ed6f7d91a11fd11d
FritzFlorian <flo.fritz@t-online.de>
2017-08-21 22:35:38 +0200

    Refactor token_validation

--- lib/fire_auth/token_validation.ex
+++ lib/fire_auth/token_validation.ex
@@ -1,32 +1,45 @@
 defmodule FireAuth.TokenValidation do
-  require Logger
+  @moduledoc """
+  Validation of firebase id_tokens.
+  """
   require FireAuth.RecordHelper
 
   alias FireAuth.RecordHelper
 
+  @doc """
+  Validates a give token_string.
+  This checks if the string was signed properly and is still valid.
+
+  If this is true
+  {:ok, 
+    %{name: name, id: id, email: email, email_verified: email_verified, provider_id: provider_id}}
+  is returned.
+
+  In case there are any problems a error in the form
+  {:error, error_message}
+  is returned.
+  """
   def validate_token(token_string) do
     token = Joken.token(token_string)
     header = Joken.peek_header(token)
 
-    case verify_token(token, header) do
-      {:ok, %{error: nil, claims: claims}} ->
-        if check_token_claims(claims) do
-          {:ok, %{
-            name: claims["name"],
-            id: claims["sub"],
-            email: claims["email"],
-            email_verified: claims["email_verified"]
-          }}
-        else
-          {:error, %{message: "Token claims are invalid. (The token might be  experienced)"}}
-        end
-      {:ok, %{error: error}} ->
-        {:error, %{message: "Token verifikation failed.", reason: error}}
-      {:error, error} ->
-        {:error, %{message: error}}
+    with {:ok, claims} <- verify_token(token, header) do
+      if check_token_claims(claims) do
+        IO.inspect(claims)
+        {:ok, %{
+          name: claims["name"],
+          id: claims["sub"],
+          email: claims["email"],
+          email_verified: claims["email_verified"],
+          provider_id: claims["provider_id"]
+        }}
+      else
+        {:error, "Token claims are invalid. (The token might be  experienced or the project_id might be wrong)"}
+      end
     end
   end
 
+  # checks that all requirements are met for the token claims
   defp check_token_claims(claims) do
     check_token_claims_exp(claims) &&
     check_token_claims_iat(claims) &&
@@ -39,27 +52,24 @@ defmodule FireAuth.TokenValidation do
   defp check_token_claims_aud(claims), do: project_id() == claims["aud"]
   defp check_token_claims_iss(claims), do: "https://securetoken.google.com/#{project_id()}" == claims["iss"]
 
+  # verifies a token using the keybase fetched from firebase.
+  # returns
+  #   {:ok, claims} when the token was verified successfully
+  #   {:error, error_message} otherwise
   defp verify_token(token, %{"alg" => "RS256", "kid" => kid}) do
-    cert = Map.get(FireAuth.KeyServer.get_keybase() ,kid)
+    cert = Map.get(FireAuth.KeyServer.get_keybase(), kid)
 
     case cert do
       nil ->
-        Logger.debug("Could not find public certificate matching token kid.")
         {:error, "Could not find public certificate matching token kid."}
       cert ->
-        Logger.debug("Certificate Found. Verifying token.")
-
-        # Read the certificate
-        pemEntries = :public_key.pem_decode(cert)
-        {_, certEntry} = :lists.keysearch(:Certificate, 1, pemEntries)
-        {_, derCert, _} = certEntry
-        decoded = :public_key.pkix_decode_cert(derCert, :otp)
-
-        otp_certificate = RecordHelper.otp_certificate(decoded, :tbsCertificate)
-        public_key_info = RecordHelper.otptbs_certificate(otp_certificate, :subjectPublicKeyInfo)
-        public_key = RecordHelper.otp_subject_public_key_info(public_key_info, :subjectPublicKey)
-
-        jwk = JOSE.JWK.from_key(public_key)
+        jwk =
+          cert
+          |> decode_cert() # decode the cert read from googles json
+          |> RecordHelper.otp_certificate(:tbsCertificate) # use records to get the part we need
+          |> RecordHelper.otptbs_certificate(:subjectPublicKeyInfo)
+          |> RecordHelper.otp_subject_public_key_info(:subjectPublicKey)
+          |> JOSE.JWK.from_key() # create our JWK token form it
 
         # Validate the token
         # This returns the token with possible verify errors
@@ -67,14 +77,29 @@ defmodule FireAuth.TokenValidation do
                           |> Joken.with_signer(Joken.rs256(jwk))
                           |> Joken.verify
 
-        {:ok, verified_token}
+        case verified_token do
+          %{error: nil, claims: claims} ->
+            {:ok, claims}
+          %{error: error} ->
+            {:error, "Token verifikation failed. #{inspect(error)}"}
+          _ ->
+            {:error, "Token verifikation failed. Unkonwn result."}
+        end
     end
   end
+
   defp verify_token(_, _) do
     {:error, "Wrong algorithm in token header."}
   end
 
+  # decodes the certificate with the kid given in the token id
+  defp decode_cert(cert) do
+    [{:Certificate, cert_entry, :not_encrypted}] = :public_key.pem_decode(cert)
+    :public_key.pkix_decode_cert(cert_entry, :otp)
+  end
+
   defp project_id() do
-    Application.get_env(:fire_auth, :project_id)
+    Application.get_env(:fire_auth, :project_id) ||
+      raise ":fire_auth, :project_id not set! Please add it to your config file."
   end
 end
